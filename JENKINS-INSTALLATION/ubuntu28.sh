74b3d16de4c69489325b4e4efd37eedcac6dd7e4
Jimmy Tita <ndungdrey@gmail.com>
2023-05-12 02:24:59 +0100

    update

---
+++ JENKINS-INSTALLATION/ubuntu28.sh
@@ -0,0 +1,18 @@
+# run script as root user
+sudo -i 
+# Jenkins Installation And Setup In AWS EC2 ubuntu Instance.
+# Installation of Java
+sudo apt update   # Update the repositories
+sudo apt install openjdk-11-jdk
+java -version
+# Install Jenkins
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install jenkins -y 
+sudo systemctl enable jenkins 
+sudo systemctl start jenkins 
+
